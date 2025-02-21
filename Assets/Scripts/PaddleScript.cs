2c21c679e372eae3cf6f2c70d0eeeca5d2ba20db
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-21 05:05:41 -0600

    added two player pong controls for the paddle

--- Assets/Scripts/PaddleScript.cs
+++ Assets/Scripts/PaddleScript.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class PaddleScript : MonoBehaviour
 {
+    public float speed = 10f;
+    public float boundaryY = 4.0f;
+    
+    public float moveInput;
+
+    public KeyCode upKey = KeyCode.W;
+    public KeyCode downKey = KeyCode.S;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float move = 0;
+
+        if(Input.GetKey(upKey)) {
+            move = 1;
+        }
+        else if(Input.GetKey(downKey)) {
+            move = -1;
+        }
+
+        transform.Translate(Vector3.up * move * speed * Time.deltaTime);
+
+        float clampedY = Mathf.Clamp(transform.position.y, -boundaryY, boundaryY);
+        transform.position = new Vector3(
+            transform.position.x, 
+            clampedY, 
+            transform.position.z
+        );
     }
 }
