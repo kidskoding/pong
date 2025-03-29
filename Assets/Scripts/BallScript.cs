02bdc606dba255e19acd9898c70943742217325d
abhay-harpalani <abhay.harpalani@gmail.com>
2025-03-29 18:25:27 -0500

    changed ball collision behavior

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,6 +1,11 @@
+using Mono.Cecil;
+using Unity.Mathematics;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BallScript : MonoBehaviour {
+    // public float speed = 20f;
     public float speed = 10f;
     public float launchDelay = 2f;
     private Rigidbody2D rb;
@@ -46,8 +51,9 @@ public class BallScript : MonoBehaviour {
     void LaunchBall() {
         if(!hasStarted) {
             float xSpeed = 1f;
-            float val = Random.Range(-1f, 1f);
-            if(val < 0) {
+            // float val = UnityEngine.Random.Range(-1f, 1f);
+			float val = -1f;
+            if (val < 0) {
               xSpeed = -1f;
             }
 
@@ -61,11 +67,42 @@ public class BallScript : MonoBehaviour {
     }
     
     void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.CompareTag("Wall")) {
+        if (collision.gameObject.CompareTag("Wall")) {
             direction.y = -direction.y;
         }
-        if(collision.gameObject.CompareTag("Paddle")) {
-            direction.x = -direction.x;
+
+        if (collision.gameObject.CompareTag("Paddle")) {
+			bool final_vector_points_right = direction.x < 0;
+			direction.x = -Mathf.Abs(direction.x);
+
+			// dtheta is the change in angle (based on relative position of ball on paddle)
+			float dtheta = (transform.position.y - collision.gameObject.transform.position.y) / 1.5f;
+			// Debug.Log("normalized dtheta");
+			// Debug.Log(dtheta);
+			// ^ normalized to [-1f, 1f]
+
+			dtheta *= Mathf.PI / 8f;
+			// ^ normalized to [-PI/8, PI/8]
+			
+			float theta_initial = Mathf.Atan2(direction.y, direction.x);
+			// Debug.Log("theta_initial");
+			// Debug.Log(theta_initial);
+			float theta_final = theta_initial + dtheta;
+
+			// Debug.Log("theta_final before");
+			// Debug.Log(theta_final);
+
+			theta_final = Mathf.Clamp(theta_final, -Mathf.PI*3/8, Mathf.PI*3/8);
+
+			// Debug.Log("theta_final after");
+			// Debug.Log(theta_final);
+
+			if (final_vector_points_right) {
+				direction.x = Mathf.Cos(theta_final);
+			} else {
+				direction.x = -Mathf.Cos(theta_final);
+			}
+			direction.y = Mathf.Sin(theta_final);
         }
     }
 }
