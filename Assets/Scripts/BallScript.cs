aef4236fa11ec540543353b1c677a73ea7c775ef
abhay-harpalani <abhay.harpalani@gmail.com>
2025-04-03 18:09:48 -0500

    bugfixes for ball collision

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -51,8 +51,7 @@ public class BallScript : MonoBehaviour {
     void LaunchBall() {
         if(!hasStarted) {
             float xSpeed = 1f;
-            // float val = UnityEngine.Random.Range(-1f, 1f);
-			float val = -1f;
+            float val = UnityEngine.Random.Range(-1f, 1f);
             if (val < 0) {
               xSpeed = -1f;
             }
@@ -72,30 +71,22 @@ public class BallScript : MonoBehaviour {
         }
 
         if (collision.gameObject.CompareTag("Paddle")) {
+			// Debug.Log("initial direction.x, direction.y");
+			// Debug.Log(direction.x);
+			// Debug.Log(direction.y);
+
 			bool final_vector_points_right = direction.x < 0;
-			direction.x = -Mathf.Abs(direction.x);
+			direction.x = Mathf.Abs(direction.x);
 
 			// dtheta is the change in angle (based on relative position of ball on paddle)
 			float dtheta = (transform.position.y - collision.gameObject.transform.position.y) / 1.5f;
-			// Debug.Log("normalized dtheta");
-			// Debug.Log(dtheta);
-			// ^ normalized to [-1f, 1f]
-
-			dtheta *= Mathf.PI / 8f;
-			// ^ normalized to [-PI/8, PI/8]
 			
 			float theta_initial = Mathf.Atan2(direction.y, direction.x);
-			// Debug.Log("theta_initial");
-			// Debug.Log(theta_initial);
 			float theta_final = theta_initial + dtheta;
 
-			// Debug.Log("theta_final before");
-			// Debug.Log(theta_final);
 
-			theta_final = Mathf.Clamp(theta_final, -Mathf.PI*3/8, Mathf.PI*3/8);
-
-			// Debug.Log("theta_final after");
-			// Debug.Log(theta_final);
+			float max_ball_angle = Mathf.PI/2;
+			theta_final = Mathf.Clamp(theta_final, -max_ball_angle, max_ball_angle);
 
 			if (final_vector_points_right) {
 				direction.x = Mathf.Cos(theta_final);
