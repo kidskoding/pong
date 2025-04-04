5efeddd1932dfd6e0520bc58b2c72843576048cd
Anirudh Konidala <kidskodingclub@gmail.com>
2025-04-03 22:33:15 -0500

    an appropriate action can be taken, but ball movement is still pretty broken

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,11 +1,6 @@
-using Mono.Cecil;
-using Unity.Mathematics;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class BallScript : MonoBehaviour {
-    // public float speed = 20f;
     public float speed = 10f;
     public float launchDelay = 2f;
     private Rigidbody2D rb;
@@ -14,8 +9,7 @@ public class BallScript : MonoBehaviour {
     private bool hasStarted = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         if(rb == null) {
             rb = GetComponent<Rigidbody2D>();
         }
@@ -24,8 +18,7 @@ public class BallScript : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if(!hasStarted) {
             Vector3 screenCenter = new Vector3(
                 Screen.width / 2,
@@ -43,17 +36,23 @@ public class BallScript : MonoBehaviour {
         }
 
         Vector3 screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
-        if(screenPos.x > Screen.width || screenPos.x < 0) {
-          hasStarted = false;
+
+        var paddleAi = GameObject.FindWithTag("AIPaddle").GetComponent<PaddleAgentScript>();
+        if(screenPos.x > Screen.width) {
+            paddleAi.AddReward(-1.0f);
+            hasStarted = false;
+        } else if(screenPos.x < 0) {
+            paddleAi.AddReward(+1.0f);
+            hasStarted = false;
         }
     }
 
     void LaunchBall() {
         if(!hasStarted) {
             float xSpeed = 1f;
-            float val = UnityEngine.Random.Range(-1f, 1f);
-            if (val < 0) {
-              xSpeed = -1f;
+            float val = Random.Range(-1f, 1f);
+            if(val < 0) {
+                xSpeed = -1f;
             }
 
             direction = new Vector2(
@@ -66,34 +65,34 @@ public class BallScript : MonoBehaviour {
     }
     
     void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Wall")) {
+        if(collision.gameObject.CompareTag("Wall")) {
             direction.y = -direction.y;
         }
 
-        if (collision.gameObject.CompareTag("Paddle")) {
-			// Debug.Log("initial direction.x, direction.y");
-			// Debug.Log(direction.x);
-			// Debug.Log(direction.y);
+        if(collision.gameObject.CompareTag("Paddle") || collision.gameObject.CompareTag("AIPaddle")) {
+            // Debug.Log("initial direction.x, direction.y");
+            // Debug.Log(direction.x);
+            // Debug.Log(direction.y);
 
-			bool final_vector_points_right = direction.x < 0;
-			direction.x = Mathf.Abs(direction.x);
+            bool final_vector_points_right = direction.x < 0;
+            direction.x = Mathf.Abs(direction.x);
 
-			// dtheta is the change in angle (based on relative position of ball on paddle)
-			float dtheta = (transform.position.y - collision.gameObject.transform.position.y) / 1.5f;
-			
-			float theta_initial = Mathf.Atan2(direction.y, direction.x);
-			float theta_final = theta_initial + dtheta;
+            // dtheta is the change in angle (based on relative position of ball on paddle)
+            float dtheta = (transform.position.y - collision.gameObject.transform.position.y) / 1.5f;
+            
+            float theta_initial = Mathf.Atan2(direction.y, direction.x);
+            float theta_final = theta_initial + dtheta;
 
+            float max_ball_angle = Mathf.PI/2;
+            theta_final = Mathf.Clamp(theta_final, -max_ball_angle, max_ball_angle);
 
-			float max_ball_angle = Mathf.PI/2;
-			theta_final = Mathf.Clamp(theta_final, -max_ball_angle, max_ball_angle);
+            if (final_vector_points_right) {
+                direction.x = Mathf.Cos(theta_final);
+            } else {
+                direction.x = -Mathf.Cos(theta_final);
+            }
 
-			if (final_vector_points_right) {
-				direction.x = Mathf.Cos(theta_final);
-			} else {
-				direction.x = -Mathf.Cos(theta_final);
-			}
-			direction.y = Mathf.Sin(theta_final);
+            direction.y = Mathf.Sin(theta_final);
         }
     }
 }
--- Assets/Scripts/MLPaddleAgentScript.cs
+++ Assets/Scripts/MLPaddleAgentScript.cs
@@ -7,60 +7,93 @@ public class PaddleAgentScript : Agent {
     public float speed = 5f;
     private GameObject ball;
     private Rigidbody2D ballRb;
-    private Rigidbody2D paddleRb;    
+    
+    public bool debugMode = true;
+    public float verticalBoundary = 4.5f;
+    
+    void Start() {
+        Initialize();
+    }
     
     public override void Initialize() {
         ball = GameObject.Find("Ball");
-        paddleRb = ball.GetComponent<Rigidbody2D>();
-        ballRb = ball.GetComponent<Rigidbody2D>();
-    }
-
-    /// Collect the states of the paddle
-    ///
-    /// State Space
-    ///   - paddle position
-    ///   - ball position
-    ///   - ball velocity (includes direction)
-    public override void CollectObservations(VectorSensor sensor) {
-        sensor.AddObservation(this.transform.position);
-        sensor.AddObservation(ball.transform.position);
-        sensor.AddObservation(ball.GetComponent<Rigidbody2D>().linearVelocity);
-    }
-    
-    /// Control the paddle agent's action 
-    ///
-    /// Discrete Action Space
-    ///   - Do nothing
-    ///   - Move Up 
-    ///   - Move Down
-    ///
-    /// Train the Paddle Agent AI based on its action, 
-    /// where it gains or loses reward based on whether the appropriate action was done
-    public override void OnActionReceived(ActionBuffers actions) {
-        int action = actions.DiscreteActions[0];
-
-        switch(action) {
-            case 0:
-                break;
-            case 1:
-                ballRb.linearVelocity = new Vector2(0, speed);
-                break;
-            case 2:
-                ballRb.linearVelocity = new Vector2(0, -speed);
-                break;
+        if(ball != null) {
+            ballRb = ball.GetComponent<Rigidbody2D>();
+        } else {
+            Debug.LogError("Ball not found. Make sure it's named 'Ball' in the scene.");
         }
     }
-
-    /// Set environment state when the episode begins
-    ///
-    /// Typically would reset all states
+    
+    // Update is called once per frame to show debug visuals
+    void Update() {
+        if(debugMode) {
+            Debug.Log($"Action: Last action taken, Reward: {GetCumulativeReward()}");
+            
+            if(ball != null) {
+                Debug.DrawLine(transform.position, ball.transform.position, Color.red);
+            }
+        }
+    }
+    
+    public override void CollectObservations(VectorSensor sensor) {
+        float normalizedPaddleY = transform.position.y / verticalBoundary;
+        sensor.AddObservation(normalizedPaddleY);
+        
+        if(ball != null) {
+            Vector3 ballRelativePos = ball.transform.position - transform.position;
+            sensor.AddObservation(ballRelativePos.x / 10f);
+            sensor.AddObservation(ballRelativePos.y / 10f);
+            
+            Vector2 ballVelocity = ballRb.linearVelocity.normalized;
+            sensor.AddObservation(ballVelocity.x);
+            sensor.AddObservation(ballVelocity.y);
+        } else {
+            sensor.AddObservation(0f);
+            sensor.AddObservation(0f);
+            sensor.AddObservation(0f);
+            sensor.AddObservation(0f);
+        }
+    }
+    
+    public override void OnActionReceived(ActionBuffers actions) {
+        int action = actions.DiscreteActions[0];
+        Debug.LogWarning($"Agent chose action: {action}");
+        
+        Vector3 paddlePosition = transform.position;
+        
+        switch(action) {
+            case 0:
+                Debug.Log("0 is chosen");
+                break;
+            case 1:
+                Debug.Log("1 is chosen");
+                paddlePosition.y += speed * Time.deltaTime;
+                break;
+            case 2:
+                Debug.Log("2 is chosen");
+                paddlePosition.y -= speed * Time.deltaTime;
+                break;
+        }
+        
+        paddlePosition.y = Mathf.Clamp(paddlePosition.y, -verticalBoundary, verticalBoundary);
+        transform.position = paddlePosition;
+    }
+    
+    /* public override void Heuristic(in ActionBuffers actionsOut) {
+        var discreteActions = actionsOut.DiscreteActions;
+        
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            discreteActions[0] = 1;
+        } else if(Input.GetKey(KeyCode.DownArrow)) {
+            discreteActions[0] = 2;
+        } else {
+            discreteActions[0] = 0;
+        }
+    } */
+    
     public override void OnEpisodeBegin() {
-        transform.position = new Vector3(transform.position.x, 0, 0);
-        ball.transform.position = Vector3.zero;
-        ballRb.linearVelocity = Vector2.zero;
-
-        float randomAngle = Random.Range(-45f, 45f);
-        float direction = Random.Range(0, 2) == 0 ? -1f : 1f;
-        ballRb.linearVelocity = new Vector2(5f * direction, 5f * Mathf.Tan(randomAngle * Mathf.Deg2Rad));
+        if(ball != null) {
+            Debug.Log("Episode beginning. Resetting agent state.");
+        }
     }
 }
